8e784ebe516ac3873ce77a84ab03f885181c8329
Laurent Stéphenne <laurent@guibi.dev>
2025-02-01 16:47:30 -0500

    Kinda works

---
+++ unity/Assets/Scripts/ServerConnection.cs
@@ -0,0 +1,93 @@
+using NativeWebSocket;
+using System;
+using System.Text;
+using UnityEngine;
+
+public class ServerConnection : MonoBehaviour
+{
+    private WebSocket websocket;
+    private readonly string serverUrl = "ws://helpful-blatantly-koi.ngrok-free.app/meta";
+    //private readonly string serverUrl = "ws://echo.websocket.in";
+
+    private async void Start()
+    {
+        websocket = new WebSocket(serverUrl);
+
+        websocket.OnOpen += () => Debug.Log("Server Connected");
+        websocket.OnClose += (code) => Debug.Log("Server Disconnected: " + code);
+        websocket.OnError += (error) => Debug.LogError("WebSocket Error: " + error);
+        websocket.OnMessage += (bytes) =>
+        {
+            string message = Encoding.UTF8.GetString(bytes);
+            Debug.Log("Received: " + message);
+            var json = JsonUtility.FromJson<JsonData>(message);
+            OnMessageReceived(json.type, message);
+        };
+
+        await websocket.Connect();
+    }
+
+
+    private async void OnDestroy()
+    {
+        if (websocket != null)
+        {
+            await websocket.Close();
+        }
+    }
+
+    private async void Update()
+    {
+        if (websocket.State == WebSocketState.Closed || websocket.State == WebSocketState.Closing)
+        {
+            Debug.Log("Reconnecting...");
+            await websocket.Connect();
+        }
+
+#if !UNITY_WEBGL || UNITY_EDITOR
+        if (websocket.State == WebSocketState.Open)
+        {
+            websocket.DispatchMessageQueue();
+        }
+#endif
+    }
+
+    private void OnMessageReceived(string type, string message)
+    {
+        switch (type)
+        {
+            case "spawn":
+                var data = JsonUtility.FromJson<SpawnJsonData>(message);
+                break;
+        }
+    }
+
+    public async void SendJson(object obj)
+    {
+        if (websocket.State == WebSocketState.Open)
+        {
+            string message = JsonUtility.ToJson(obj);
+            await websocket.SendText(message);
+            Debug.Log("Sent to WebSocket: " + message);
+        }
+        else
+        {
+            Debug.Log("Unable to send message to WebSocket.");
+        }
+    }
+
+    [Serializable]
+    private class JsonData
+    {
+        public string type;
+    }
+
+    [Serializable]
+    private class SpawnJsonData : JsonData
+    {
+        public string name;
+        public int x;
+        public int y;
+        public string meshyId;
+    }
+}
